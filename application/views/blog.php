b8a9f8638f8f40f557c280e5b359705702c0ea8b
Andrian Febriansyah Istianto <87926420+NauseaSB@users.noreply.github.com>
2022-07-14 10:25:39 +0700

    Add files via upload

---
+++ application/views/blog.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Saya</title>
+</head>
+
+<body>
+    selamat datang <?php echo $nama; ?>!
+    <br>
+    kamu merupakan kelas <?php echo $kelas; ?>!
+    <br>
+    dengan nim <?php echo $nim; ?>!
+</body>
+
+</html>
